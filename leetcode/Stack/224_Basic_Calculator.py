7c90cc028e6c1177a8aeaecd74c41e30d7a3c5e3
harisha-viswa <harishaviswa@gmail.com>
2024-07-15 13:32:29 +0530

    jul 10

---
+++ leetcode/Stack/224_Basic_Calculator.py
@@ -0,0 +1,55 @@
+'''
+Type : hard
+
+Problem statement:
+
+Given a string s representing a valid expression, implement a basic calculator to evaluate it, and return the result of the evaluation.
+Note: You are not allowed to use any built-in function which evaluates strings as mathematical expressions, such as eval().
+
+Example 1:
+
+Input: s = "1 + 1"
+Output: 2
+Example 2:
+
+Input: s = " 2-1 + 2 "
+Output: 3
+Example 3:
+
+Input: s = "(1+(4+5+2)-3)+(6+8)"
+Output: 23
+ 
+'''
+class basic_calculator():
+    def calculate(self, s: str) -> int:
+  
+        stack = []
+        current_number = 0
+        result = 0
+        sign = 1
+
+        for char in s:
+            if char.isdigit():
+                current_number = current_number * 10 + int(char)
+            elif char == '+':
+                result += sign * current_number
+                current_number = 0
+                sign = 1
+            elif char == '-':
+                result += sign * current_number
+                current_number = 0
+                sign = -1
+            elif char == '(':
+                stack.append(result)
+                stack.append(sign)
+                result = 0
+                sign = 1
+            elif char == ')':
+                result += sign * current_number
+                current_number = 0
+                result *= stack.pop() 
+                result += stack.pop() 
+        result += sign * current_number
+        return result
+solution =basic_calculator()
+print(solution.calculate("1 + 1"))
